24bc65652ec3063bbad656d8c1f9da701caa7707
unknown <hoemsomnangrupp@gmail.com>
2023-05-24 15:55:28 +0700

    first commit

---
+++ src/main/java/AES/aes/AESMain.java
@@ -0,0 +1,9 @@
+package AES.aes;
+
+public class AESMain {
+
+	public static void main(String[] args) throws Exception {
+		
+	}
+
+}
---
+++ src/test/java/AES/aes/AesApplicationTests.java
@@ -0,0 +1,13 @@
+package AES.aes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
